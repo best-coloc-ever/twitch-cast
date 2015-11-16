f729a9010b8e3e5abc54f7d521a19f558e93c9ab
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-11-17 00:01:56 +0100

    Added a log file for livestreamer's output

--- streamer/stream.sh
+++ streamer/stream.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 url=$1
+log_file="/livestreamer.log"
 
 livestreamer \
     $url \
     best \
     --player-external-http \
     --player-external-http-port 4242 \
-    --yes-run-as-root
+    --yes-run-as-root &> $log_file
