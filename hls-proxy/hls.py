b0ffa13491d0895ed4237d27e8255f99cd10b09d
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-11-19 00:26:52 +0100

    Added a configuration key for the server's video path

--- hls-proxy/hls.py
+++ hls-proxy/hls.py
@@ -5,6 +5,7 @@ import subprocess
 SERVER_HOSTNAME         = os.environ['SERVER_HOSTNAME']
 STREAMER_HOSTNAME       = os.environ['STREAMER_HOSTNAME']
 OUTPUT_DIRECTORY        = os.environ['OUTPUT_DIRECTORY']
+SERVER_PATH             = os.environ['SERVER_PATH']
 OUTPUT_INDEX_FILE_NAME  = os.environ['OUTPUT_INDEX_FILE_NAME']
 OUTPUT_TS_FILE_PATTERN  = os.environ['OUTPUT_TS_FILE_PATTERN']
 SEGMENT_LEN             = 12 # Seconds
@@ -16,32 +17,33 @@ SOUT_CONFIG_TEMPLATE = '\
         delsegs = true,\
         numsegs = 10,\
         index = {out_dir}/{idx_file},\
-        index-url = http://{hostname}/video/{ts_pattern}\
+        index-url = http://{host_name}/{host_path}/{ts_pattern}\
     }},\
     mux = ts{{\
         use-key-frames\
     }},\
-    dst = /data/video/{ts_pattern}\
+    dst = {out_dir}/{ts_pattern}\
 }}'
 
-def sout_config(subdir):
+def sout_config(local_path, server_path):
     raw = SOUT_CONFIG_TEMPLATE.format(
         seg_len=SEGMENT_LEN,
-        out_dir=os.path.join(OUTPUT_DIRECTORY, subdir),
+        out_dir=local_path,
         idx_file=OUTPUT_INDEX_FILE_NAME,
-        hostname=SERVER_HOSTNAME,
+        host_name=SERVER_HOSTNAME,
+        host_path=server_path,
         ts_pattern=OUTPUT_TS_FILE_PATTERN
     )
     # Removing whitespaces because of VLC's weird parsing rules
     return ''.join(raw.split())
 
-VLC_COMMAND = lambda port: [
+VLC_COMMAND = lambda port, local_path, server_path: [
     'vlc',
     '-I', 'dummy',
     '--play-and-exit',
     '--live-caching', '300',
     'http://{}:{}'.format(STREAMER_HOSTNAME, port),
-    '--sout', sout_config('{}'.format(port))
+    '--sout', sout_config(local_path, server_path)
 ]
 
 class Proxy:
@@ -51,8 +53,20 @@ class Proxy:
         self.port = port
 
     def start(self):
+        subdir = '{}'.format(self.port)
+        local_path = os.path.join(OUTPUT_DIRECTORY, subdir)
+        server_path = os.path.join(SERVER_PATH, subdir)
+
+        os.makedirs(local_path)
+
+        command = VLC_COMMAND(
+            self.port,
+            local_path,
+            server_path
+        )
+
         self.process = subprocess.Popen(
-            VLC_COMMAND(self.port),
+            command,
             stdout=sys.stderr.fileno(),
             stderr=subprocess.PIPE,
         )
