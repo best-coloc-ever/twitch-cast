2d88773b21694c851cd98f15798e1570578ec32a
Ubuntu-14.04.3-64-bits <guillaume.depardon@outlook.com>
2015-11-21 02:41:40 +0100

    Made the HLS API consistent with the streamer one

--- hls-proxy/api.py
+++ hls-proxy/api.py
@@ -1,65 +1,78 @@
 #!/usr/bin/env python
 
+from hls import Proxy
+
+hls_proxies = dict()
+
+def poll_proxies():
+    dead_proxies = [
+        id for id, proxy in hls_proxies.items()
+        if not proxy.alive()
+    ]
+
+    for id in dead_proxies:
+        del hls_proxies[id]
+
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-import hls
-import os
-
-hls_proxies = set()
-
-def poll_proxies():
-    dead_proxies = [
-        proxy for proxy in hls_proxies
-        if not proxy.alive()
-    ]
-
-    for proxy in dead_proxies:
-        hls_proxies.remove(proxy)
+from utils import validate_json_request, preprocess
 
 @app.route('/proxies', methods=['GET'])
+@preprocess(poll_proxies)
 def proxies():
-    poll_proxies()
-
     return jsonify(
         proxies=[
             proxy.to_json()
-            for proxy in hls_proxies
+            for proxy in hls_proxies.values()
         ]
     )
 
-@app.route('/watch', methods=['POST'])
-def watch():
-    port = request.form['port']
+@app.route('/proxy/<int:proxy_id>', methods=['GET'])
+@preprocess(poll_proxies)
+def proxy(proxy_id):
+    try:
+        proxy = hls_proxies[proxy_id]
+    except KeyError:
+        return jsonify(
+            errors=['proxy with id {} does not exist'.format(proxy_id)]
+        ), 404
 
-    poll_proxies()
+    return jsonify(
+        proxy=proxy.to_json()
+    )
 
-    proxy = hls.Proxy(port)
-    if proxy in hls_proxies:
+@app.route('/proxies', methods=['POST'])
+@validate_json_request('watch')
+@preprocess(poll_proxies)
+def watch(payload):
+    stream_id = payload['stream_id']
+
+    try:
+        hls_proxies[stream_id]
         status = 'ALREADY_WATCHING'
-    else:
+    except KeyError:
+        proxy = Proxy(stream_id)
         proxy.start()
-        hls_proxies.add(proxy)
+        hls_proxies[stream_id] = proxy
         status = 'OK'
 
     return jsonify(
         status=status
     )
 
-@app.route('/unwatch', methods=['POST'])
-def unwatch():
-    port = request.form['port']
-
-    proxy = hls.Proxy(port)
-    if proxy in hls_proxies:
-        hls_proxies.remove(proxy)
-        status = 'OK'
-    else:
-        status = 'NOT_FOUND'
+@app.route('/proxy/<int:proxy_id>', methods=['DELETE'])
+def unwatch(proxy_id):
+    try:
+        del hls_proxies[proxy_id]
+    except KeyError:
+        return jsonify(
+            errors=['proxy with id {} does not exist'.format(proxy_id)]
+        ), 404
 
     return jsonify(
-        status=status
+        status='OK'
     )
 
 if __name__ == '__main__':
