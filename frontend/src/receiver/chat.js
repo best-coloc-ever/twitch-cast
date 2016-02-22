d1fe7827b7fb032d2d49a92e80eb61f14e30bb28
Guillaume Depardon <guillaume.depardon@gmail.com>
2016-02-22 02:31:11 +0100

    Now using proper queuing to display chat Also clearing more gently Should improve performance

--- frontend/src/receiver/chat.js
+++ frontend/src/receiver/chat.js
@@ -78,14 +78,19 @@ var ChatAssetStore = function() {
   });
 }
 
-var CHAT_DELAY = 0; // seconds
+var CHAT_DISPLAY_INTERVAL = 0.25; // seconds
+var CHAT_CLEAR_INTERVAL = 10; // seconds
+var CHAT_DELAY = 10; // seconds
 
 var currentChannel = null;
 var chatSocket = null;
 
+var chatMessagesQueue = [];
+var chatDisplayId = setInterval(displayMessages, CHAT_DISPLAY_INTERVAL * 1000);
+var chatClearId = setInterval(clearChat, CHAT_CLEAR_INTERVAL * 1000);
+
 var chatContainer = $('#chat');
 var chatLines = $('#chat-lines');
-var chatLinesCount = 0;
 
 var chatAssetStore = new ChatAssetStore();
 
@@ -100,7 +105,6 @@ function displayChatRaw(text) {
 
 function connectToChat(channel) {
   chatAssetStore.fetchBadges(channel);
-  chatLinesCount = 0;
 
   // Close any previous connection
   if (chatSocket)
@@ -115,6 +119,7 @@ function connectToChat(channel) {
 
   chatSocket.onopen = function(e) {
     currentChannel = channel;
+
     displayChatRaw('Successfully joined the channel!');
     displayChatRaw('Delaying chat for: ' + CHAT_DELAY + ' seconds')
   };
@@ -124,12 +129,35 @@ function connectToChat(channel) {
   };
 
   chatSocket.onmessage = function(e) {
-    var message = JSON.parse(e.data);
-    setTimeout(displayPrivateMessage.bind(null, message), CHAT_DELAY * 1000);
+    var message = makeChatLine(JSON.parse(e.data));
+    chatMessagesQueue.push(message);
   };
 }
 
-function displayPrivateMessage(message) {
+function displayMessages() {
+  var i = 0;
+  var now = new Date().getTime();
+
+  for (; i < chatMessagesQueue.length; ++i) {
+    var message = chatMessagesQueue[i];
+
+    if (now - message.stamp < CHAT_DELAY * 1000)
+      break ;
+
+    chatLines.append(message);
+  }
+
+  chatMessagesQueue = chatMessagesQueue.slice(i, chatMessagesQueue.length);
+
+  chatContainer.scrollTop(chatContainer[0].scrollHeight);
+}
+
+function clearChat() {
+  // Keeping the chat relatively small
+  chatLines.find('li:lt(-100)').remove();
+}
+
+function makeChatLine(message) {
   var color = message.tags ? message.tags.color : 'blue';
 
   var line = $('<li>')
@@ -140,19 +168,14 @@ function displayPrivateMessage(message) {
     .css('color', color);
   var contentSpan = makeContentSpan(message.content);
 
-  prependBadges(message.tags, line);
+  if (message.tags)
+    prependBadges(message.tags, line);
   line.append(nameSpan);
   line.append(contentSpan);
-  chatLines.append(line);
 
-  // Keeping the chat relatively small
-  chatLinesCount++;
-  while (chatLinesCount > 100) {
-    chatLines.find('li:first').remove();
-    chatLinesCount--;
-  }
+  line.stamp = new Date().getTime();
 
-  chatContainer.scrollTop(chatContainer[0].scrollHeight);
+  return line;
 }
 
 function makeContentSpan(content) {
@@ -166,29 +189,26 @@ function makeContentSpan(content) {
 }
 
 function prependBadges(tags, line) {
-  if (tags) {
-    var specialAttributes = ['mod', 'subscriber', 'turbo']
-      .filter(function(attribute) {
-        return (tags[attribute] == '1');
-      });
+  var specialAttributes = ['mod', 'subscriber', 'turbo']
+    .filter(function(attribute) {
+      return (tags[attribute] == '1');
+    });
 
-    if (tags['user-type'])
-      specialAttributes.push(tags['user-type']);
+  if (tags['user-type'])
+    specialAttributes.push(tags['user-type']);
 
-    if (specialAttributes.length > 0) {
-      var badgeWrapper = $('<span>')
-        .addClass('badge-wrapper');
-      var badgeContainer = $('<div>');
+  if (specialAttributes.length > 0) {
+    var badgeWrapper = $('<span>')
+      .addClass('badge-wrapper');
+    var badgeContainer = $('<div>');
 
-      $.unique(specialAttributes).forEach(function(attribute) {
-        var badge = $('<img>')
-          .attr('src', chatAssetStore.badges[currentChannel][attribute]);
-        badgeContainer.append(badge);
-      })
+    $.unique(specialAttributes).forEach(function(attribute) {
+      var badge = $('<img>')
+        .attr('src', chatAssetStore.badges[currentChannel][attribute]);
+      badgeContainer.append(badge);
+    })
 
-      badgeWrapper.wrapInner(badgeContainer);
-      line.append(badgeWrapper);
-    }
-
+    badgeWrapper.wrapInner(badgeContainer);
+    line.append(badgeWrapper);
   }
 }
