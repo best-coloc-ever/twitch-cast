756e440a200e6e50fbc78c1450e93aa21903d9d6
Guillaume Depardon <guillaume.depardon@gmail.com>
2016-04-01 05:43:22 +0200

    Moved and simplified stuff

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
 
-set -eu
-
-dc() {
-    docker-compose --x-networking "$@"
-}
-
-# Data volumes
-docker volume create --name "twitch-cast-data"         # Video storage
-docker volume create --name "twitch-cast-sender-dist"  # Prod website storage
-docker volume create --name "twitch-cast-sender-dev"   # Dev website storage
-docker volume create --name "twitch-cast-node-modules" # Node package cache
+set -e
 
 # Asset pipeline
-dc run --rm frontend-builder npm install
-dc run --rm frontend-builder bower install --allow-root
-dc run --rm frontend-builder gulp dist
+docker-compose run --rm sender-builder npm install
+docker-compose run --rm sender-builder bower install
+docker-compose run --rm sender-builder gulp dist
 
 # Magic
-dc up -d
+docker-compose up -d
