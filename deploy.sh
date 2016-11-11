9930d0b13e45b0dc66a9437a74fd2cc0a91cbfa4
Guillaume Depardon <guillaume.depardon@gmail.com>
2016-11-11 04:25:08 +0100

    Added the compilation step in the deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,10 @@ receiver-run() {
   ./scripts/dev run --rm receiver-builder $@
 }
 
+streamer-run() {
+  ./scripts/dev run --rm streamer-builder $@
+}
+
 # Bundle the sender application
 sender-run npm install
 sender-run bower install
@@ -18,6 +22,9 @@ sender-run gulp dist
 # Bundle the receiver application
 receiver-run npm install
 receiver-run webpack -p
+
+# Build the streamer app
+streamer-run stack build --copy-bins --local-bin-path /dist
 
 # Magic
 ./scripts/prod up -d
